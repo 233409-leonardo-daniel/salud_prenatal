0fb9ec4731f609b09e4249f7e1c32db660a3f6ff
AdrianGzn <233318@ids.upchiapas.edu.mx>
2026-07-29 10:35:21 -0600

    device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'app.dart';
@@ -15,7 +16,7 @@ void main() async {
 
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: kIsWeb, // Solo en web
       builder: (context) => const MyApp(),
     ),
   );
